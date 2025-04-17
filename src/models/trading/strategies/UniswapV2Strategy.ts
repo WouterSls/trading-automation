1f67a146a2b2f59f89ddebb01333cf39dfe189df
woutersls <phaerocode@hotmail.com>
2025-04-17 20:44:09 +0200

    chore: liquidity functionality implementation

--- src/models/trading/strategies/UniswapV2Strategy.ts
+++ src/models/trading/strategies/UniswapV2Strategy.ts
@@ -1,18 +1,21 @@
-import { Wallet, ethers } from "ethers";
+import { TransactionLike, TransactionRequest, Wallet, ethers } from "ethers";
 import { ChainType } from "../../../config/chain-config";
-import { UniswapV2Router } from "../../uniswap-v2/UniswapV2Router";
-import { AbstractTradingStrategy } from "./AbstractTradingStrategy";
+import { UniswapV2Router, UniswapV2Factory } from "../../uniswap-v2/index";
 import { ERC20 } from "../../ERC/ERC20";
 import { BuyTrade } from "../trades/BuyTrade";
-import { UniswapV2Factory } from "../../uniswap-v2/UniswapV2Factory";
 import { ERC20_INTERFACE } from "../../../contract-abis/erc20";
+import { ITradingStrategy } from "../ITradingStrategy";
 
-export class UniswapV2Strategy extends AbstractTradingStrategy {
+export class UniswapV2Strategy implements ITradingStrategy {
   private router: UniswapV2Router;
   private factory: UniswapV2Factory;
 
+  private strategyName: string;
+  private chain: ChainType;
+
   constructor(STRATEGY_NAME: string, chain: ChainType) {
-    super(STRATEGY_NAME, chain);
+    this.strategyName = STRATEGY_NAME;
+    this.chain = chain;
     this.router = new UniswapV2Router(chain);
     this.factory = new UniswapV2Factory(chain);
   }
@@ -20,26 +23,66 @@ export class UniswapV2Strategy extends AbstractTradingStrategy {
   /**
    * Getters
    */
+  getName = (): string => this.strategyName;
+  getChain = (): ChainType => this.chain;
+
   async getETHLiquidity(wallet: Wallet, tokenAddress: string): Promise<string> {
     const pairAddress: string | null = await this.factory!.getPairAddress(wallet, tokenAddress);
-    if (!pairAddress) throw new Error(`No pair found for ${tokenAddress} and ${this.router!.getWETHAddress()}`);
+    if (!pairAddress) throw new Error(`No pair found for ${tokenAddress} and ${this.factory!.getWETHAddress()}`);
 
-    const weth = new ethers.Contract(this.router!.getWETHAddress(), ERC20_INTERFACE, wallet);
+    const weth = new ethers.Contract(this.factory!.getWETHAddress(), ERC20_INTERFACE, wallet);
     const ethLiquidity = await weth.balanceOf(pairAddress);
     const ethLiquidityFormatted = ethers.formatEther(ethLiquidity);
     return ethLiquidityFormatted;
+  }
+
+  async getUsdcPrice(wallet: Wallet, tokenAddress: string): Promise<string> {
+    throw new Error("Not implemented");
+    return "0";
   }
 
   /**
    * Buy
    */
   async buy(wallet: Wallet, token: ERC20, usdAmount: number): Promise<BuyTrade> {
-    return new Promise((resolve, reject) => {
-      reject(new Error("Not implemented"));
-    });
+    let tx: TransactionRequest | TransactionLike;
+    tx = await this.router.createSwapExactETHInputTransaction(wallet, token, usdAmount);
+    console.log("tx request:", tx);
+    tx = await wallet.populateTransaction(tx);
+    console.log("populated tx:", tx);
+    throw new Error("Not implemented");
+    const txResponse = await wallet.sendTransaction(tx);
+    const txReceipt = await txResponse.wait();
   }
   async simulateBuy(wallet: Wallet, token: ERC20, usdAmount: number): Promise<boolean> {
     await this.router.simulateBuySwap(wallet, token, usdAmount);
     return true;
+  }
+
+  private async getV2Reserves(wallet: Wallet, tokenAddress: string) {
+    try {
+      const pair = await this.factory.getPair(wallet, tokenAddress);
+      const WETH_ADDRESS = this.factory.getWETHAddress();
+
+      if (pair.getAddress() === ethers.ZeroAddress) {
+        return ethers.ZeroAddress;
+      }
+
+      const reserves = await pair.getReserves();
+
+      const token0 = tokenAddress.toLowerCase() < WETH_ADDRESS!.toLowerCase() ? tokenAddress : WETH_ADDRESS;
+      const [reserveToken, reserveETH] =
+        token0 === tokenAddress ? [reserves.reserve0, reserves.reserve1] : [reserves.reserve1, reserves.reserve0];
+
+      return {
+        liquidityTokens: reserveToken.toString(),
+        liquidityEth: reserveETH.toString(),
+        pairAddress: pair.getAddress(),
+      };
+    } catch (error: unknown) {
+      const errorMessage = error instanceof Error ? error.message : "Unknown error";
+      console.error(`${tokenAddress} - ${errorMessage}`);
+      return ethers.ZeroAddress;
+    }
   }
 }
