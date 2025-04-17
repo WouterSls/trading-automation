acb03170a06b685dc10f04dd54f5f2ff3afd813c
woutersls <phaerocode@hotmail.com>
2025-04-17 20:45:15 +0200

    feat: uniswap V2 factory abstraction

--- src/models/uniswap-v2/UniswapV2Factory.ts
+++ src/models/uniswap-v2/UniswapV2Factory.ts
@@ -1,39 +1,50 @@
-import { Contract, Wallet } from "ethers";
+import { Contract, ethers, Wallet } from "ethers";
 import { UNISWAP_V2_FACTORY_INTERFACE } from "../../contract-abis/uniswap-v2";
 import { ChainType, getChainConfig } from "../../config/chain-config";
+import { validateNetwork } from "../../lib/utils";
+import { UniswapV2Pair } from "./UniswapV2Pair";
 
 export class UniswapV2Factory {
   private factoryContract: Contract;
   private WETH_ADDRESS: string;
 
-  constructor(chain: ChainType) {
+  constructor(private chain: ChainType) {
     const chainConfig = getChainConfig(chain);
     this.WETH_ADDRESS = chainConfig.tokenAddresses.weth;
 
     this.factoryContract = new Contract(chainConfig.uniswapV2.factoryAddress, UNISWAP_V2_FACTORY_INTERFACE);
   }
 
+  getWETHAddress = (): string => this.WETH_ADDRESS;
+
   /**
    * Validates that the address is actually a Uniswap V2 Factory by checking if it implements the required interface
+   * @param wallet Wallet instance -> blockchain provider
+   * @returns true if the initialized factory address is a valid Uniswap V2 Factory, false otherwise
    */
   async validateIsFactory(wallet: Wallet): Promise<boolean> {
     try {
-      console.log("validating factory...");
-      // Attempt to call a method that should exist on the factory contract with dummy arguments
-      // This won't execute the transaction but will fail if the method doesn't exist
+      await validateNetwork(wallet, this.chain);
+
+      this.factoryContract = this.factoryContract.connect(wallet) as Contract;
+
       await this.factoryContract.getPair.staticCall(
         "0x0000000000000000000000000000000000000001",
         "0x0000000000000000000000000000000000000002",
       );
-      console.log("factory validated");
       return true;
-    } catch (error: any) {
-      // If the error is NOT about the method not existing, then it's likely a valid factory
-      if (!error.message.includes("function selector was not recognized")) {
-        return true;
+    } catch (error: unknown) {
+      const errorMessage = error instanceof Error ? error.message : "Unknown error";
+
+      if (errorMessage.toLowerCase().includes("cannot read properties of null")) {
+        throw new Error(`Wallet has missing provider: ${errorMessage}`);
       }
-      console.error("Address validation failed, not a valid Uniswap V2 Factory:", error);
-      return false;
+
+      if (errorMessage.includes("missing provider")) {
+        throw new Error(`Wallet has missing provider: ${errorMessage}`);
+      }
+
+      throw new Error(`${errorMessage}`);
     }
   }
 
@@ -47,38 +58,28 @@ export class UniswapV2Factory {
    * so getPair(tokenA, tokenB) and getPair(tokenB, tokenA) will return the same pair address.
    * Internally, the token with the lower address becomes token0 and the higher address becomes token1.
    */
-  async getPairAddress(wallet: Wallet, tokenAddress: string): Promise<string | null> {
-    // Validate the factory first
-    console.log("getting pair address...");
-    console.log("connecting factory...");
+  async getPairAddress(wallet: Wallet, tokenAddress: string): Promise<string> {
     this.factoryContract = this.factoryContract.connect(wallet) as Contract;
-    console.log("factory connected");
 
     const isValid = await this.validateIsFactory(wallet);
     if (!isValid) {
       throw new Error(`Address ${this.factoryContract.address} is not a valid Uniswap V2 Factory`);
     }
 
-    // Use staticCall to get the pair address since it's a view function
-
     const pairAddress = await this.factoryContract.getPair.staticCall(tokenAddress, this.WETH_ADDRESS);
 
-    // If the pair doesn't exist, return null
-    if (pairAddress === "0x0000000000000000000000000000000000000000") {
-      return null;
+    if (pairAddress === ethers.ZeroAddress) {
+      return ethers.ZeroAddress;
     }
-
-    /**
-    // Create a new pair instance
-    const pair = new UniswapV2Pair(wallet, pairAddress);
-
-    // Validate that it's actually a pair
-    const isPair = await pair.validateIsPair();
-    if (!isPair) {
-      throw new Error(`Factory returned invalid pair address: ${pairAddress}`);
-    }
- */
 
     return pairAddress;
+  }
+
+  async getPair(wallet: Wallet, tokenAddress: string): Promise<UniswapV2Pair> {
+    const pairAddress = await this.getPairAddress(wallet, tokenAddress);
+    if (!pairAddress) {
+      throw new Error("Pair not found");
+    }
+    return new UniswapV2Pair(wallet, pairAddress);
   }
 }
