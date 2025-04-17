83f5f61a815dbbd4c59d1e5ddccdfcc69abca62e
woutersls <phaerocode@hotmail.com>
2025-04-17 20:45:00 +0200

    chore: liquidity implementation & start on buy functionality

--- src/models/uniswap-v2/UniswapV2Router.ts
+++ src/models/uniswap-v2/UniswapV2Router.ts
@@ -9,24 +9,20 @@ import {
 
 import { ChainConfig, getChainConfig, ChainType } from "../../config/chain-config";
 import { ERC20 } from "../ERC/ERC20";
-import { TradeSuccessInfo } from "../../lib/types/trading.types";
+import { TradeSuccessInfo } from "../trading/trades/AbstractBaseTrade";
 
 import { UNISWAP_V2_ROUTER_INTERFACE } from "../../contract-abis/uniswap-v2";
 import { calculateSlippageAmount, extractRawTokenOutputFromLogs } from "../../lib/utils";
-import { TRADING_CONFIG } from "../../config/setup-config";
-import { ITradingStrategy } from "../trading/strategies/interface/ITradingStrategy";
+import { TRADING_CONFIG } from "../../hooks/useSetup";
 
 export class UniswapV2Router {
-  private readonly NAME = "Uniswap V2 Router";
-  private isInitialized = false;
-
   //Addresses
-  private wethAddress: string = "";
-  private udscAddress: string = "";
-  private routerAddress: string = "";
+  private wethAddress: string;
+  private udscAddress: string;
+  private routerAddress: string;
 
   //Contract
-  private routerContract: Contract | null = null;
+  private routerContract: Contract;
 
   //Constants
   private readonly WEI_DECIMALS = 18;
@@ -54,78 +50,18 @@ export class UniswapV2Router {
     }
 
     this.routerContract = new ethers.Contract(this.routerAddress, UNISWAP_V2_ROUTER_INTERFACE);
-
-    this.isInitialized = true;
   }
 
-  getName = (): string => this.NAME;
   getWETHAddress = (): string => this.wethAddress;
 
   /**
    *
-   * @pricing
-   *
-   */
-  private async getEthUsdcPrice() {
-    if (!this.isInitialized) throw new Error(`${this.NAME} not initialized`);
-    const tradePath = [this.wethAddress, this.udscAddress];
-    const inputAmount = ethers.parseUnits("1", this.WETH_DECIMALS);
-
-    const amountsOut = await this.routerContract!.getAmountsOut(inputAmount, tradePath);
-    const amountOut = amountsOut[amountsOut.length - 1];
-    const amountFormatted = ethers.formatUnits(amountOut.toString(), this.USDC_DECIMALS);
-    return parseFloat(amountFormatted);
-  }
-  private async getTokenPriceUsdc(token: ERC20): Promise<number> {
-    try {
-      if (!this.isInitialized) throw new Error(`${this.NAME} not initialized`);
-
-      const tradePath = [token.getTokenAddress(), this.wethAddress, this.udscAddress];
-      const inputAmount = ethers.parseUnits("1", token.getDecimals());
-
-      const amountsOut: BigInt[] = await this.routerContract!.getAmountsOut(inputAmount, tradePath);
-      const amountOut = amountsOut[amountsOut.length - 1];
-
-      const amountFormatted = ethers.formatUnits(amountOut.toString(), this.USDC_DECIMALS);
-
-      return parseFloat(amountFormatted);
-    } catch (error: unknown) {
-      console.error(`Error getting token ${token.getName()} USDC price on UniV2Router: `, error);
-      if (error instanceof Error) throw error;
-      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
-      throw new Error(`UniV2Router token ${token.getName()} USDC price error: ${errorMessage}`);
-    }
-  }
-  private async getTokenPriceEth(token: ERC20): Promise<number> {
-    try {
-      if (!this.isInitialized) throw new Error("Router not initialized");
-
-      const tradePath = [token.getTokenAddress(), this.wethAddress];
-
-      const inputAmount = ethers.parseUnits("1", token.getDecimals());
-
-      const amountsOut: BigInt[] = await this.routerContract!.getAmountsOut(inputAmount, tradePath);
-      const amountOut = amountsOut[amountsOut.length - 1];
-
-      const amountFormatted = ethers.formatUnits(amountOut.toString(), this.WETH_DECIMALS);
-      return parseFloat(amountFormatted);
-    } catch (error: unknown) {
-      console.error(`Error getting token ${token.getName()} ETH price on UniV2Router: `, error);
-      if (error instanceof Error) throw error;
-      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
-      throw new Error(`UniV2Router token ${token.getName()} price error: ${errorMessage}`);
-    }
-  }
-
-  /**
-   *
-   * @buying
+   * Public methods
+   * Connect wallet at start of each method
    *
    */
   async simulateBuySwap(wallet: Wallet, token: ERC20, usdValue: number): Promise<void> {
     try {
-      if (!this.isInitialized) throw new Error("Router not initialized");
-
       this.routerContract = this.routerContract!.connect(wallet) as Contract;
 
       const { minOutputAmount, path, deadline, value } = await this.prepareBuySwapParameters(token, usdValue);
@@ -140,10 +76,9 @@ export class UniswapV2Router {
       throw new Error(`Transaction simulation failed: ${errorMessage}`);
     }
   }
+
   async swapEthInUsdForToken(wallet: Wallet, token: ERC20, usdValue: number): Promise<TradeSuccessInfo> {
     try {
-      if (!this.isInitialized) throw new Error(`${this.NAME} not initialized`);
-
       this.routerContract = this.routerContract!.connect(wallet) as Contract;
 
       const { minOutputAmount, path, deadline, value } = await this.prepareBuySwapParameters(token, usdValue);
@@ -211,6 +146,93 @@ export class UniswapV2Router {
       throw new Error(errorMessage);
     }
   }
+
+  /**
+   * Creates an ethers transaction request for swapExactETHForTokens function
+   * @param wallet - The wallet to trade with
+   * @param token - The ERC20 token to buy
+   * @param usdAmount - The amount of USD to buy
+   * @returns The transaction request
+   */
+  async createSwapExactETHInputTransaction(
+    wallet: Wallet,
+    token: ERC20,
+    usdAmount: number,
+  ): Promise<TransactionRequest> {
+    this.routerContract = this.routerContract!.connect(wallet) as Contract;
+
+    const { minOutputAmount, path, deadline, value } = await this.prepareBuySwapParameters(token, usdAmount);
+
+    const abiInterface = new ethers.Interface([
+      "function swapExactETHForTokens(uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external payable returns (uint256[] memory amounts)",
+    ]);
+
+    const encodedData = abiInterface.encodeFunctionData("swapExactETHForTokens", [
+      minOutputAmount,
+      path,
+      wallet.address,
+      deadline,
+    ]);
+
+    const tx: TransactionRequest = {
+      to: this.routerAddress,
+      data: encodedData,
+      value: value,
+    };
+
+    return tx;
+  }
+
+  /**
+   *
+   * Private methods
+   */
+  private async getEthUsdcPrice() {
+    const tradePath = [this.wethAddress, this.udscAddress];
+    const inputAmount = ethers.parseUnits("1", this.WETH_DECIMALS);
+
+    const amountsOut = await this.routerContract!.getAmountsOut(inputAmount, tradePath);
+    const amountOut = amountsOut[amountsOut.length - 1];
+    const amountFormatted = ethers.formatUnits(amountOut.toString(), this.USDC_DECIMALS);
+    return parseFloat(amountFormatted);
+  }
+  private async getTokenPriceUsdc(token: ERC20): Promise<number> {
+    try {
+      const tradePath = [token.getTokenAddress(), this.wethAddress, this.udscAddress];
+      const inputAmount = ethers.parseUnits("1", token.getDecimals());
+
+      const amountsOut: BigInt[] = await this.routerContract!.getAmountsOut(inputAmount, tradePath);
+      const amountOut = amountsOut[amountsOut.length - 1];
+
+      const amountFormatted = ethers.formatUnits(amountOut.toString(), this.USDC_DECIMALS);
+
+      return parseFloat(amountFormatted);
+    } catch (error: unknown) {
+      console.error(`Error getting token ${token.getName()} USDC price on UniV2Router: `, error);
+      if (error instanceof Error) throw error;
+      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+      throw new Error(`UniV2Router token ${token.getName()} USDC price error: ${errorMessage}`);
+    }
+  }
+  private async getTokenPriceEth(token: ERC20): Promise<number> {
+    try {
+      const tradePath = [token.getTokenAddress(), this.wethAddress];
+
+      const inputAmount = ethers.parseUnits("1", token.getDecimals());
+
+      const amountsOut: BigInt[] = await this.routerContract!.getAmountsOut(inputAmount, tradePath);
+      const amountOut = amountsOut[amountsOut.length - 1];
+
+      const amountFormatted = ethers.formatUnits(amountOut.toString(), this.WETH_DECIMALS);
+      return parseFloat(amountFormatted);
+    } catch (error: unknown) {
+      console.error(`Error getting token ${token.getName()} ETH price on UniV2Router: `, error);
+      if (error instanceof Error) throw error;
+      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+      throw new Error(`UniV2Router token ${token.getName()} price error: ${errorMessage}`);
+    }
+  }
+
   private async estimateUsdValueToTokenTrade(
     _token: ERC20,
     _usdValue: number,
@@ -283,40 +305,181 @@ export class UniswapV2Router {
 
   /**
    *
-   * @transactions
+   * @selling
+   * TODO: Implement
    *
    */
+  /** 
+  async simulateSellSwap(erc20: ERC20, rawSellAmount: bigint): Promise<void> {
+    try {
+      const rawBalance = await erc20.getRawTokenBalance(this.walletAddress);
+      const allowedBalance = await erc20.getRawAllowance(this.walletAddress, this.routerAddress);
 
-  /**
-   * Creates a buy transaction
-   * @param receiverAddress - The address of the receiver
-   * @param token - The ERC20 token to buy
-   * @param usdAmount - The amount of USD to buy
-   * @returns The transaction request
-   */
-  async createBuyTransaction(receiverAddress: string, token: ERC20, usdAmount: number): Promise<TransactionRequest> {
-    /** 
-    const { minOutputAmount, path, deadline, value } = await this.prepareBuySwapParameters(token, usdAmount);
+      if (rawBalance <= 0n) {
+        throw new Error(`No token balance for: ${erc20.getName()}`);
+      }
 
-    const abiInterface = new ethers.Interface([
-      "function swapExactETHForTokens(uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external payable returns (uint256[] memory amounts)",
-    ]);
-    const encodedData = abiInterface.encodeFunctionData("swapExactETHForTokens", [
-      minOutputAmount,
-      path,
-      receiverAddress,
-      deadline,
-    ]);
+      if (allowedBalance <= 0n) {
+        throw new Error(`No allowance for token: ${erc20.getName()}`);
+      }
 
-    const tx: TransactionRequest = {
-      to: this.routerAddress,
-      data: encodedData,
-      value: value,
-    };
+      if (allowedBalance < rawSellAmount) {
+        throw new Error(
+          `Insufficient allowance for token: ${erc20.getName()} | allowed: ${ethers.formatUnits(
+            allowedBalance,
+            erc20.getDecimals(),
+          )} - sell amount: ${ethers.formatUnits(rawSellAmount, erc20.getDecimals())}`,
+        );
+      }
 
-    return tx;
+      const path = [erc20.getTokenAddress(), this.wethAddress];
+      const to = this.walletAddress;
+      const deadline = Math.floor(Date.now() / 1000) + 60 * this.TRADING_DEADLINE_MINUTES;
 
-*/
-    throw new Error("Not implemented");
+      const amountsOut = await this.routerContract!.getAmountsOut(rawSellAmount, path);
+
+      const expectedOutput: bigint = amountsOut[amountsOut.length - 1];
+      const amountOutMin = (expectedOutput * 95n) / 100n;
+
+      await this.routerContract!.swapExactTokensForETH.staticCall(rawSellAmount, amountOutMin, path, to, deadline);
+
+      console.log(`Transaction simulation for method: swapExactTokensForETH passed | sell simulation`);
+    } catch (error: any) {
+      console.error(`Simulation failed for swapExactTokensForETH:`, error.message);
+      const errorMessage = error.toString().includes("reverted") ? error.toString() : "Unknown simulation error";
+      throw new Error(`Transaction simulation failed: ${errorMessage}`);
+    }
   }
+  async swapExactTokenForEth(token: ERC20, rawSellAmount: bigint): Promise<TradeSuccessInfo> {
+    try {
+      if (!this.isInitialized) throw new Error("Router not initialized");
+      const rawBalance = await token.getRawTokenBalance(this.walletAddress);
+      const allowedBalance = await token.getRawAllowance(this.walletAddress, this.routerAddress);
+
+      if (rawBalance === 0n) {
+        throw new Error(`No balance for token: ${token.getName()}`);
+      }
+
+      if (allowedBalance === 0n) {
+        throw new Error(`No allowance for token: ${token.getName()}`);
+      }
+
+      if (allowedBalance < rawSellAmount) {
+        throw new Error(
+          `Insufficient allowance for token: ${token.getName()} | allowed: ${ethers.formatUnits(
+            allowedBalance,
+            token.getDecimals(),
+          )} - sell amount: ${ethers.formatUnits(rawSellAmount, token.getDecimals())}`,
+        );
+      }
+
+      if (rawSellAmount <= 0n) {
+        throw new Error(`Insufficient sell amount: ${ethers.formatUnits(rawSellAmount, token.getDecimals())}`);
+      }
+
+      if (rawSellAmount > rawBalance) {
+        throw new Error(
+          `Insufficient balance for token: ${token.getName()} | sell amount: ${ethers.formatUnits(
+            rawSellAmount,
+            token.getDecimals(),
+          )} - balance: ${ethers.formatUnits(rawBalance, token.getDecimals())}`,
+        );
+      }
+
+      const { rawTokensReceived, priceImpact } = await this.estimateTokenToEthTrade(token, rawSellAmount);
+
+      if (priceImpact > TRADING_CONFIG.MAX_PRICE_IMPACT_PERCENTAGE) {
+        throw new Error(
+          `Price impact too high: ${priceImpact}%, max allowed: ${TRADING_CONFIG.MAX_PRICE_IMPACT_PERCENTAGE}%`,
+        );
+      }
+
+      const slippageAmount = calculateSlippageAmount(rawTokensReceived, TRADING_CONFIG.SLIPPAGE_TOLERANCE);
+      const minOutputTokens = rawTokensReceived - slippageAmount;
+
+      const deadline = Math.floor(Date.now() / 1000) + 60 * this.TRADING_DEADLINE_MINUTES;
+      const tradePath = [token.getTokenAddress(), this.wethAddress];
+
+      await this.simulateSellSwap(token, rawSellAmount);
+
+      const txResponse: ContractTransactionResponse = await this.routerContract!.swapExactTokensForETH(
+        rawSellAmount,
+        minOutputTokens,
+        tradePath,
+        this.walletAddress,
+        deadline,
+      );
+
+      const txReceipt: ContractTransactionReceipt | null = await txResponse.wait();
+      if (!txReceipt) throw new Error("Transaction receipt not found");
+
+      const transactionStatus = txReceipt.status;
+      if (transactionStatus !== 1) {
+        throw new Error("Transaction failed: execution reverted, ran out of gas, or encountered another error");
+      }
+
+      const transactionHash = txResponse.hash;
+      const confirmedBlock = txReceipt.blockNumber;
+      const gasCost = txReceipt.gasPrice * txReceipt.gasUsed;
+      const gasCostFormatted = ethers.formatEther(gasCost);
+      const logs = txReceipt.logs;
+
+      const ethReceivedWei = logs[logs.length - 1].data;
+      const ethReceived = parseFloat(ethers.formatEther(ethReceivedWei));
+
+      const tokenPriceUsd = await this.getTokenPriceUsdc(token);
+      const ethPriceUsd = await this.getEthUsdcPrice();
+
+      return {
+        transactionHash: transactionHash,
+        confirmedBlock: confirmedBlock,
+        gasCost: gasCostFormatted,
+        rawTokensSpent: rawSellAmount.toString(),
+        formattedTokensSpent: ethers.formatUnits(rawSellAmount.toString(), token.getDecimals()),
+        ethReceived: ethReceived.toString(),
+        tokenPriceUsd: tokenPriceUsd.toString(),
+        ethPriceUsd: ethPriceUsd.toString(),
+      };
+    } catch (error: any) {
+      console.error(`Transaction failed for swapExactTokensForETH:`, error.message);
+      const errorMessage = error.toString().includes("reverted") ? error.toString() : "Unknown simulation error";
+      throw new Error(`Transaction failed: ${errorMessage}`);
+    }
+  }
+  private async estimateTokenToEthTrade(
+    token: ERC20,
+    rawSellAmount: bigint,
+  ): Promise<{ rawTokensReceived: bigint; priceImpact: number }> {
+    try {
+      if (!this.isInitialized) throw new Error("Router not initialized");
+
+      const tokenPrice = await this.getTokenPriceEth(token);
+
+      const tradePath = [token.getTokenAddress(), this.wethAddress];
+
+      const amountsOut: BigInt[] = await this.routerContract!.getAmountsOut(rawSellAmount, tradePath);
+      const amountOut = amountsOut[amountsOut.length - 1];
+
+      const rawTokensReceived = BigInt(amountOut.toString());
+
+      const formattedTokensReceived = ethers.formatUnits(amountOut.toString(), this.WETH_DECIMALS);
+      const tokensReceived = parseFloat(formattedTokensReceived);
+
+      const formattedSellAmount = ethers.formatUnits(rawSellAmount.toString(), token.getDecimals());
+      const sellAmount = parseFloat(formattedSellAmount);
+
+      const priceImpact = ((tokenPrice - tokensReceived / sellAmount) / tokenPrice) * 100;
+
+      return {
+        rawTokensReceived: rawTokensReceived,
+        priceImpact: Math.max(0, priceImpact),
+      };
+    } catch (error: unknown) {
+      console.error("Estimation error for sell swap on UniV2Router | selling estimation");
+      if (error instanceof Error) throw error;
+      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+      throw new Error(`UniV2Router sell estimation error: ${errorMessage}`);
+    }
+  }
+*/
 }
