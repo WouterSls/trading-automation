29634e90f7c727db6841f63323ee4ff9eabdc293
wsels <phaerocode@hotmail.com>
2025-04-22 12:23:08 +0200

    chore: add router abi

--- src/contract-abis/uniswap-v3.ts
+++ src/contract-abis/uniswap-v3.ts
@@ -25,6 +25,15 @@ const V3_FACTORY_ABI = [
   "function createPool(address tokenA, address tokenB, uint24 fee) external returns (address pool)",
 ] as const;
 
+const V3_ROUTER_ABI = [
+  // Core swapping functions
+  "function exactInputSingle(tuple(address tokenIn, address tokenOut, uint24 fee, address recipient, uint256 deadline, uint256 amountIn, uint256 amountOutMinimum, uint160 sqrtPriceLimitX96)) external payable returns (uint256 amountOut)",
+  "function exactInput(tuple(bytes path, address recipient, uint256 deadline, uint256 amountIn, uint256 amountOutMinimum)) external payable returns (uint256 amountOut)",
+  "function exactOutputSingle(tuple(address tokenIn, address tokenOut, uint24 fee, address recipient, uint256 deadline, uint256 amountOut, uint256 amountInMaximum, uint160 sqrtPriceLimitX96)) external payable returns (uint256 amountIn)",
+  "function exactOutput(tuple(bytes path, address recipient, uint256 deadline, uint256 amountOut, uint256 amountInMaximum)) external payable returns (uint256 amountIn)",
+] as const;
+
 export const POOL_ABI = new ethers.Interface(V3_POOL_ABI);
 export const QUOTER_ABI = new ethers.Interface(V2_QUOTER_ABI);
 export const FACTORY_ABI = new ethers.Interface(V3_FACTORY_ABI);
+export const ROUTER_ABI = new ethers.Interface(V3_ROUTER_ABI);
