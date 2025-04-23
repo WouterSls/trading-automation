79439e4913939757a25605c3aea14bb6eecd1c5e
wsels <phaerocode@hotmail.com>
2025-04-23 19:05:23 +0200

    feat: add hardhat for live fork testing

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1,
+      forking: {
+        url: process.env.ETH_RPC_URL!, // e.g. Alchemy/Infura
+        blockNumber: 22_332_168, // pin to a block for determinism & caching :contentReference[oaicite:1]{index=1}
+      },
+    },
+  },
+};
+export default config;
