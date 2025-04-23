cf891f1b934d2333a86fb2df5052608fe43753ac
wsels <phaerocode@hotmail.com>
2025-04-23 19:06:23 +0200

    feat: add WETH abi

--- src/contract-abis/erc20.ts
+++ src/contract-abis/erc20.ts
@@ -10,4 +10,18 @@ const MINIMAL_ERC20_ABI = [
   "function allowance(address owner, address spender) view returns (uint256)",
 ] as const;
 
+const WETH_ABI = [
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function decimals() view returns (uint8)",
+  "function totalSupply() view returns (uint256)",
+  "function balanceOf(address) view returns (uint256)",
+  "function approve(address, uint256) external returns (bool)",
+  "function allowance(address owner, address spender) view returns (uint256)",
+  "function transferFrom(address, address, uint256) external returns (bool)",
+  "function deposit() payable",
+  "function withdraw(uint256 amount)",
+] as const;
+
 export const ERC20_INTERFACE = new ethers.Interface(MINIMAL_ERC20_ABI);
+export const WETH_INTERFACE = new ethers.Interface(WETH_ABI);
