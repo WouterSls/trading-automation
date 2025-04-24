9cbdc41a53f310207d00504fca97dae418052e4e
wsels <phaerocode@hotmail.com>
2025-04-24 12:12:30 +0200

    feat: add log decoding function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,8 @@ import { ethers, Provider, Wallet } from "ethers";
 import { ERC20_INTERFACE } from "../contract-abis/erc20";
 import { ERC20 } from "../models/ERC/ERC20";
 import { ChainType, mapNetworkNameToChainType } from "../config/chain-config";
+import { POOL_INTERFACE } from "../contract-abis/uniswap-v3";
+import { calculatePriceFromSqrtPriceX96 } from "../models/uniswap-v3/uniswap-v3-utils";
 
 export async function createMinimalErc20(address: string, provider: Provider): Promise<ERC20> {
   const contract = new ethers.Contract(address, ERC20_INTERFACE, provider);
@@ -68,4 +70,71 @@ export async function validateNetwork(wallet: Wallet, chainType: ChainType) {
   if (chain !== chainType) {
     throw new Error(`Wallet and factory are on different networks`);
   }
+}
+
+export function decodeLogs(logs: ReadonlyArray<ethers.Log>) {
+  const decodedLogs = [];
+
+  for (const log of logs) {
+    try {
+      if (log.topics[0] === ERC20_INTERFACE.getEvent("Transfer")!.topicHash) {
+        const decoded = ERC20_INTERFACE.parseLog({
+          topics: log.topics,
+          data: log.data,
+        });
+
+        if (!decoded) throw new Error("Failed to decode ERC20 Transfer");
+
+        decodedLogs.push({
+          type: "ERC20 Transfer",
+          contract: log.address,
+          from: decoded.args.from,
+          to: decoded.args.to,
+          amount: decoded.args.amount,
+          formattedAmount: ethers.formatUnits(
+            decoded.args.amount,
+            log.address.toLowerCase() === "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48" ? 6 : 18,
+          ),
+        });
+      } else if (log.topics[0] === POOL_INTERFACE.getEvent("Swap")!.topicHash) {
+        const decoded = POOL_INTERFACE.parseLog({
+          topics: log.topics,
+          data: log.data,
+        });
+
+        if (!decoded) throw new Error("Failed to decode Uniswap V3 Swap");
+
+        const sqrtPriceX96 = decoded.args.sqrtPriceX96;
+        const price = calculatePriceFromSqrtPriceX96(sqrtPriceX96);
+
+        decodedLogs.push({
+          type: "Uniswap V3 Swap",
+          pool: log.address,
+          sender: decoded.args.sender,
+          recipient: decoded.args.recipient,
+          amount0: decoded.args.amount0,
+          amount1: decoded.args.amount1,
+          sqrtPriceX96: sqrtPriceX96,
+          price,
+          liquidity: decoded.args.liquidity,
+          tick: decoded.args.tick,
+        });
+      } else {
+        decodedLogs.push({
+          type: "Unknown",
+          address: log.address,
+          topics: log.topics,
+          data: log.data,
+        });
+      }
+    } catch (error) {
+      decodedLogs.push({
+        type: "Error decoding",
+        log,
+        error,
+      });
+    }
+  }
+
+  return decodedLogs;
 }
