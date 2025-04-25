8e967e1df823d8077802aac03516ca0e276c5cb5
wsels <phaerocode@hotmail.com>
2025-04-25 20:42:42 +0200

    chore: update index

--- src/contract-abis/_index.ts
+++ src/contract-abis/_index.ts
@@ -1,3 +1,5 @@
 export * from "./erc20";
-export * from "./uniswap-v3";
 export * from "./uniswap-v2";
+export * from "./uniswap-v3";
+export * from "./uniswap-v4";
+export * from "./universal-router";
