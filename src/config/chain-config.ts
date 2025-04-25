8312cc834f9a286382adccd72c6a14465af13db4
wsels <phaerocode@hotmail.com>
2025-04-25 20:42:19 +0200

    chore: update chain config interface and values

--- src/config/chain-config.ts
+++ src/config/chain-config.ts
@@ -15,17 +15,23 @@ export interface ChainConfig {
     usdc: string;
     dai: string;
   };
-  uniswapV2: {
-    factoryAddress: string;
-    routerAddress: string;
-  };
-  uniswapV3: {
-    factoryAddress: string;
-    quoterV2Address: string;
-    tickLensAddress: string;
-    swapRouterV2Address: string;
+  uniswap: {
+    v2: {
+      factoryAddress: string;
+      routerAddress: string;
+    };
+    v3: {
+      factoryAddress: string;
+      quoterV2Address: string;
+      tickLensAddress: string;
+      swapRouterV2Address: string;
+      permit2Address?: string;
+    };
+    v4: {
+      poolManagerAddress: string;
+      quoterAddress: string;
+    };
     universalRouterAddress: string;
-    permit2Address?: string;
   };
   velo?: {};
 }
@@ -42,17 +48,23 @@ export const supportedChains: Record<ChainType, ChainConfig> = {
       usdc: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
       dai: "0x6b175474e89094c44da98b954eedeac495271d0f",
     },
-    uniswapV2: {
-      factoryAddress: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
-      routerAddress: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-    },
-    uniswapV3: {
-      factoryAddress: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
-      quoterV2Address: "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",
-      tickLensAddress: "0xbfd8137f7d1516D3ea5cA83523914859ec47F573",
-      swapRouterV2Address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+    uniswap: {
+      v2: {
+        factoryAddress: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+        routerAddress: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+      },
+      v3: {
+        factoryAddress: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+        quoterV2Address: "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",
+        tickLensAddress: "0xbfd8137f7d1516D3ea5cA83523914859ec47F573",
+        swapRouterV2Address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+        permit2Address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+      },
+      v4: {
+        poolManagerAddress: "0x000000000004444c5dc75cB358380D2e3dE08A90",
+        quoterAddress: "0x52f0e24d1c21c8a0cb1e5a5dd6198556bd9e1203",
+      },
       universalRouterAddress: "0x66a9893cc07d91d95644aedd05d03f95e1dba8af",
-      permit2Address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
     },
   },
   [ChainType.ARB]: {
@@ -66,17 +78,23 @@ export const supportedChains: Record<ChainType, ChainConfig> = {
       usdc: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
       dai: "",
     },
-    uniswapV2: {
-      factoryAddress: "0xf1D7CC64Fb4452F05c498126312eBE29f30Fbcf9",
-      routerAddress: "0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24",
-    },
-    uniswapV3: {
-      factoryAddress: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
-      quoterV2Address: "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",
-      tickLensAddress: "0xbfd8137f7d1516D3ea5cA83523914859ec47F573",
-      swapRouterV2Address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+    uniswap: {
+      v2: {
+        factoryAddress: "0xf1D7CC64Fb4452F05c498126312eBE29f30Fbcf9",
+        routerAddress: "0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24",
+      },
+      v3: {
+        factoryAddress: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+        quoterV2Address: "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",
+        tickLensAddress: "0xbfd8137f7d1516D3ea5cA83523914859ec47F573",
+        swapRouterV2Address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+        permit2Address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+      },
+      v4: {
+        poolManagerAddress: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
+        quoterAddress: "0x3972c00f7ed4885e145823eb7c655375d275a1c5",
+      },
       universalRouterAddress: "0xa51afafe0263b40edaef0df8781ea9aa03e381a3",
-      permit2Address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
     },
   },
   [ChainType.BASE]: {
@@ -90,17 +108,23 @@ export const supportedChains: Record<ChainType, ChainConfig> = {
       usdc: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
       dai: "",
     },
-    uniswapV2: {
-      factoryAddress: "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6",
-      routerAddress: "0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24",
-    },
-    uniswapV3: {
-      factoryAddress: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
-      quoterV2Address: "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a",
-      tickLensAddress: "0x0CdeE061c75D43c82520eD998C23ac2991c9ac6d",
-      swapRouterV2Address: "0x2626664c2603336E57B271c5C0b26F421741e481",
+    uniswap: {
+      v2: {
+        factoryAddress: "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6",
+        routerAddress: "0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24",
+      },
+      v3: {
+        factoryAddress: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
+        quoterV2Address: "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a",
+        tickLensAddress: "0x0CdeE061c75D43c82520eD998C23ac2991c9ac6d",
+        swapRouterV2Address: "0x2626664c2603336E57B271c5C0b26F421741e481",
+        permit2Address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
+      },
+      v4: {
+        poolManagerAddress: "0x498581ff718922c3f8e6a244956af099b2652b2b",
+        quoterAddress: "0x0d5e0f971ed27fbff6c2837bf31316121532048d",
+      },
       universalRouterAddress: "0x6ff5693b99212da76ad316178a184ab56d299b43",
-      permit2Address: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
     },
   },
 };
