cb2a4d5f6ff1b7e7a1bba2f90cec3a72b688ab4d
wsels <phaerocode@hotmail.com>
2025-04-30 15:24:15 +0200

    chore: test graphapi interaction

--- src/scripts/models/uniswap-v4/state-view-interaction.ts
+++ src/scripts/models/uniswap-v4/state-view-interaction.ts
@@ -1,10 +1,10 @@
 import { ethers, Wallet } from "ethers";
 import { ChainType, getChainConfig } from "../../../config/chain-config";
-import { getHardhatWallet_1 } from "../../../hooks/useSetup";
+import { getHardhatWallet_1, getTheGraphApi } from "../../../hooks/useSetup";
 import { PoolKey } from "../../../models/uniswap-v4/uniswap-v4-types";
 import { FeeAmount, FeeToTickSpacing } from "../../../models/uniswap-v3/uniswap-v3-types";
 import { validateNetwork } from "../../../lib/utils";
-import { computePoolId } from "../../../models/uniswap-v4/uniswap-v4-utils";
+import { computePoolId, getBestPoolKey } from "../../../models/uniswap-v4/uniswap-v4-utils";
 import { UniswapV4StateView } from "../../../models/uniswap-v4/UniswapV4StateView";
 
 export async function stateViewInteraction(chain: ChainType, wallet: Wallet) {
@@ -14,8 +14,22 @@ export async function stateViewInteraction(chain: ChainType, wallet: Wallet) {
 
   const usdcAddress = chainConfig.tokenAddresses.usdc;
   const daiAddress = chainConfig.tokenAddresses.dai;
+  const ethAddress = ethers.ZeroAddress;
+
+  const theGraphApi = getTheGraphApi();
+  const poolKeys = await theGraphApi.fetchV4PoolKeysByTokens(chain, usdcAddress, daiAddress);
+  const bestPoolKey = await getBestPoolKey(poolKeys);
+  console.log("bestPoolKey", bestPoolKey);
 
   const stateView = new UniswapV4StateView(chain);
+
+  /**
+  const poolKeys = await getAllPoolKeys(chain, usdcAddress, daiAddress);
+  console.log("poolKeys", poolKeys);
+ */
+
+
+
 
   const testKey1: PoolKey = {
     currency0: usdcAddress,
@@ -27,8 +41,8 @@ export async function stateViewInteraction(chain: ChainType, wallet: Wallet) {
 
   const poolId1 = computePoolId(testKey1);
 
-  const slot01 = await stateView.getSlot0(wallet, poolId1);
-  console.log("USDC/DAI Low fee tier slot01", slot01);
+  //const slot01 = await stateView.getSlot0(wallet, poolId1);
+  //console.log("USDC/DAI Low fee tier slot01", slot01);
 }
 
 if (require.main === module) {
