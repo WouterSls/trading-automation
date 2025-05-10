97b6743ad8622686575b11e4edfb99fff6a224d2
woutersls <phaerocode@hotmail.com>
2025-05-10 18:23:26 +0200

    test: setup scaffold

--- __test__/models/universal-router/V4Trade.test.ts
+++ __test__/models/universal-router/V4Trade.test.ts
@@ -1,12 +1,13 @@
-import { Wallet } from "ethers";
+import { ethers, Wallet } from "ethers";
 import { UniversalRouter } from "../../../src/models/universal-router/UniversalRouter";
 import { NetworkForkManager } from "../../helpers/network-fork";
-import { ChainType } from "../../../src/config/chain-config";
+import { ChainConfig, ChainType, getChainConfig } from "../../../src/config/chain-config";
 import { getHardhatWallet_1 } from "../../../src/hooks/useSetup";
 
 describe("UniversalRouter address validation", () => {
   let router: UniversalRouter;
   let wallet: Wallet;
+  let chainConfig: ChainConfig;
 
   beforeAll(async () => {
     await NetworkForkManager.startHardhatFork(ChainType.ETH);
@@ -19,5 +20,15 @@ describe("UniversalRouter address validation", () => {
 
   afterAll(async () => {
     await NetworkForkManager.cleanupHardhatFork();
+  });
+
+  describe("V4 ETH Trade", () => {
+    chainConfig = getChainConfig(ChainType.ETH);
+
+    it("ETH -> USDC trade", async () => {
+      const inputToken = ethers.ZeroAddress;
+      const outputToken = chainConfig.tokenAddresses.usdc;
+      const amount = ethers.parseEther("1");
+    });
   });
 });
