08ae41e79fae6e8dd1c267d1ac2d808b1ac85d70
woutersls <phaerocode@hotmail.com>
2025-05-13 23:06:17 +0200

    test: finish eth -> usdc trade test & start on udsc -> eth trade test

--- __test__/models/universal-router/V4Trade.test.ts
+++ __test__/models/universal-router/V4Trade.test.ts
@@ -1,17 +1,22 @@
 import { ethers, Wallet } from "ethers";
 import { UniversalRouter } from "../../../src/models/universal-router/UniversalRouter";
 import { NetworkForkManager } from "../../helpers/network-fork";
-import { ChainConfig, ChainType, getChainConfig } from "../../../src/config/chain-config";
+import { ChainType, getChainConfig } from "../../../src/config/chain-config";
 import { getHardhatWallet_1 } from "../../../src/hooks/useSetup";
+import { getLowPoolKey } from "../../../src/models/uniswap-v4/uniswap-v4-utils";
+import { CommandType } from "../../../src/models/universal-router/universal-router-types";
+import { createMinimalErc20, decodeLogs } from "../../../src/lib/utils";
+import { ERC20 } from "../../../src/models/ERC/ERC20";
+import { UniswapV2Router } from "../../../src/models/uniswap-v2/UniswapV2Router";
+describe("UniversalRouter V4 Trade Tests | ETH", () => {
+  const chain = ChainType.ETH;
+  const router: UniversalRouter = new UniversalRouter(chain);
+  const chainConfig = getChainConfig(chain);
 
-describe("UniversalRouter address validation", () => {
-  let router: UniversalRouter;
   let wallet: Wallet;
-  let chainConfig: ChainConfig;
 
   beforeAll(async () => {
-    await NetworkForkManager.startHardhatFork(ChainType.ETH);
-    router = new UniversalRouter(ChainType.ETH);
+    await NetworkForkManager.startHardhatFork(chain);
   }, 20_000);
 
   beforeEach(() => {
@@ -22,13 +27,53 @@ describe("UniversalRouter address validation", () => {
     await NetworkForkManager.cleanupHardhatFork();
   });
 
-  describe("V4 ETH Trade", () => {
-    chainConfig = getChainConfig(ChainType.ETH);
-
+  describe("Trades", () => {
     it("ETH -> USDC trade", async () => {
-      const inputToken = ethers.ZeroAddress;
-      const outputToken = chainConfig.tokenAddresses.usdc;
-      const amount = ethers.parseEther("1");
+      // Arrange
+      const usdc = await createMinimalErc20(chainConfig.tokenAddresses.usdc, wallet.provider!);
+
+      const inputToken = ethers.ZeroAddress; // ETH
+      const outputToken = usdc.getTokenAddress();
+      const inputAmount = ethers.parseEther("1");
+      const minOutputAmount = 0n;
+      const recipient = wallet.address;
+
+      const poolKey = getLowPoolKey(inputToken, outputToken);
+      const zeroForOne = poolKey.currency0 === inputToken;
+
+      const command = CommandType.V4_SWAP;
+      const input = router.encodeV4SwapInput(poolKey, zeroForOne, inputAmount.toString(), minOutputAmount, recipient);
+      const deadline = Math.floor(Date.now() / 1000) + 1200;
+
+      // Act
+      const tx = await router.createExecuteTransaction(wallet, command, [input], deadline);
+      tx.value = ethers.parseEther("10");
+      const txResponse = await wallet.sendTransaction(tx);
+      const txReceipt = await txResponse.wait();
+
+      // Assert
+      if (!txReceipt) throw new Error("Transaction failed");
+      expect(txReceipt.status).toBe(1);
+      const usdcBalance = await usdc.getFormattedTokenBalance(recipient);
+      expect(usdcBalance).toBeGreaterThan(0n);
+    });
+
+    it("USDC -> ETH trade", async () => {
+      // Arrange
+      const usdc = await createMinimalErc20(chainConfig.tokenAddresses.usdc, wallet.provider!);
+      const uniswapV2Router = new UniswapV2Router(chain);
+      await uniswapV2Router.swapEthInUsdForToken(wallet, usdc, 200);
+      const usdcBalanceBefore = await usdc.getFormattedTokenBalance(wallet.address);
+      expect(usdcBalanceBefore).toBeGreaterThan(0n);
+
+      const inputToken = usdc.getTokenAddress();
+      const outputToken = ethers.ZeroAddress;
+      const inputAmount = ethers.parseUnits("150", usdc.getDecimals());
+      const minOutputAmount = 0n;
+      const recipient = wallet.address;
+
+      const poolKey = getLowPoolKey(inputToken, outputToken);
+      const zeroForOne = poolKey.currency0 === inputToken;
     });
   });
 });
