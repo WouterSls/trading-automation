2ec5dd9a8d47c1de1179d146662512de10e3f466
woutersls <phaerocode@hotmail.com>
2025-05-19 22:58:18 +0200

    chore: fix base & arbitrum forking

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,32 +4,67 @@ import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
+const forkNetwork = process.env.FORK_NETWORK || "ethereum";
+
+let networkConfig;
+switch (forkNetwork) {
+  case "base":
+    networkConfig = {
+      forking: {
+        url: process.env.BASE_MAINNET_RPC_URL || process.env.BASE_RPC_URL!,
+        blockNumber: 30_005_952,
+      },
+      chainId: 8453,
+      chains: {
+        8453: {
+          hardforkHistory: {
+            berlin: 0,
+            london: 0,
+            merge: 0,
+            shanghai: 0,
+            //cancun: 0,
+            //prague: 0, | prague / electra -> pectra
+          },
+        },
+      },
+    };
+    break;
+  case "arbitrum":
+    networkConfig = {
+      forking: {
+        url: process.env.ARB_MAINNET_RPC_URL || process.env.ARB_RPC_URL!,
+        blockNumber: 334_908_297,
+      },
+      chainId: 42161,
+      chains: {
+        42161: {
+          hardforkHistory: {
+            berlin: 0,
+            london: 0,
+            merge: 15537393,
+            shanghai: 17034870,
+            cancun: 19426589,
+            prague: 22431084,
+          },
+        },
+      },
+    };
+    break;
+  default:
+    networkConfig = {
+      forking: {
+        url: process.env.ETH_RPC_URL!,
+        blockNumber: 22_344_527,
+      },
+      chainId: 1,
+    };
+    break;
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    hardhat: {
-      chainId: 1,
-      forking: {
-        url: process.env.ETH_RPC_URL!, // e.g. Alchemy/Infura
-        blockNumber: 22_344_527, // pin to a block for determinism & caching
-      },
-    },
-    hardhat_arb: {
-      chainId: 42161,
-      url: process.env.ARB_RPC_URL!,
-      forking: {
-        url: process.env.ARB_RPC_URL!,
-        blockNumber: 334_908_297,
-      },
-    },
-    hardhat_base: {
-      chainId: 8453,
-      url: process.env.BASE_RPC_URL!,
-      forking: {
-        url: process.env.BASE_RPC_URL!,
-        blockNumber: 30_005_952,
-      },
-    },
+    hardhat: networkConfig,
   },
   mocha: {
     timeout: 100000, // Longer timeout for fork tests
