6e4c8d4f20e534dc41e3e5de30595833c70ceeff
woutersls <phaerocode@hotmail.com>
2025-05-21 21:34:02 +0200

    chore: scaffold V4 quoter

---
+++ src/models/blockchain/uniswap-v4/UniswapV4Quoter.ts
@@ -0,0 +1,4 @@
+export class UniswapV4Quoter {
+  //https://docs.uniswap.org/contracts/v4/reference/periphery/interfaces/IV4Quoter
+  //https://etherscan.io/address/0x52f0e24d1c21c8a0cb1e5a5dd6198556bd9e1203
+}
