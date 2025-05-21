fe22776a45d581e2205fc21fbef19b767210bbf8
woutersls <phaerocode@hotmail.com>
2025-05-21 23:28:10 +0200

    chore: add UniV4 to index

--- src/models/trading/strategies/_index.ts
+++ src/models/trading/strategies/_index.ts
@@ -1,5 +1,4 @@
 export * from "./AerodromeStrategy";
-export * from "./BalancerStrategy";
-export * from "./SushiswapV2Strategy";
 export * from "./UniswapV2Strategy";
 export * from "./UniswapV3Strategy";
+export * from "./UniswapV4Strategy";
