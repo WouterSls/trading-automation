ab0ece14427b3a09752fe5a98f64e6c081756a85
woutersls <phaerocode@hotmail.com>
2025-05-21 23:11:39 +0200

    refactor: reduce trading strategy interface

--- src/models/trading/ITradingStrategy.ts
+++ src/models/trading/ITradingStrategy.ts
@@ -1,24 +1,12 @@
-import { Wallet } from "ethers";
-import { ChainType } from "../../config/chain-config";
-import { ERC20 } from "../blockchain/ERC/ERC20";
-import { BuyTrade, SellTrade, OutputToken } from "./types/_index";
+import { TransactionRequest, Wallet } from "ethers";
+import { BuyTradeCreationDto, SellTradeCreationDto } from "../../api/trades/TradesController";
 
 export interface ITradingStrategy {
-  /**
-   * Getters
-   */
   getName(): string;
-  getChain(): ChainType;
-  getETHLiquidity(wallet: Wallet, tokenAddress: string): Promise<string>;
-  getUsdcPrice(wallet: Wallet, tokenAddress: string): Promise<string>;
-  /**
-   * Buy
-   */
-  buy(wallet: Wallet, token: ERC20, usdAmount: number): Promise<BuyTrade>;
-  simulateBuy(wallet: Wallet, token: ERC20, usdAmount: number): Promise<boolean>;
-  /**
-   * Sell
-   */
-  sell(wallet: Wallet, token: ERC20, tokenAmount: number, outputToken: OutputToken): Promise<SellTrade>;
-  simulateSell(wallet: Wallet, token: ERC20, tokenAmount: number): Promise<boolean>;
+  getEthUsdcPrice(wallet: Wallet): Promise<string>;
+  getTokenEthLiquidity(wallet: Wallet, tokenAddress: string): Promise<string>;
+  getTokenUsdcPrice(wallet: Wallet, tokenAddress: string): Promise<string>;
+
+  createBuyTransaction(wallet: Wallet, trade: BuyTradeCreationDto): Promise<TransactionRequest>;
+  createSellTransaction(wallet: Wallet, trade: SellTradeCreationDto): Promise<TransactionRequest>;
 }
