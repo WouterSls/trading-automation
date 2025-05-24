73f0953afb6a77d721471e8bf12e42f66f9299bc
woutersls <phaerocode@hotmail.com>
2025-05-24 15:59:26 +0200

    feat: update sell trade to support eth sales

--- src/models/trading/types/SellTrade.ts
+++ src/models/trading/types/SellTrade.ts
@@ -7,14 +7,20 @@ export class SellTrade extends BaseTrade {
     gasCost: string,
     tokenPriceUsd: string,
     ethPriceUsd: string,
+    private ethSpent: string,
     private rawTokensSpent: string,
     private formattedTokensSpent: string,
+    private rawTokensReceived: string,
+    private formattedTokensReceived: string,
     private ethReceived: string,
   ) {
     super(transactionHash, confirmedBlock, gasCost, tokenPriceUsd, ethPriceUsd);
   }
 
+  getEthSpent = (): string => this.ethSpent;
   getRawTokensSpent = (): string => this.rawTokensSpent;
   getFormattedTokensSpent = (): string => this.formattedTokensSpent;
   getEthReceived = (): string => this.ethReceived;
+  getRawTokensReceived = (): string => this.rawTokensReceived;
+  getFormattedTokensReceived = (): string => this.formattedTokensReceived;
 }
