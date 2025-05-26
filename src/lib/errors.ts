4da0d6f4b0cb8c043fb87a1d0ea0c08d1ba94c7e
woutersls <phaerocode@hotmail.com>
2025-05-26 20:33:32 +0200

    chore: remove api errors

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -4,26 +4,3 @@ export class UserError extends Error {
     this.name = "UserError";
   }
 }
-
-export class AppError extends Error {
-  constructor(
-    public message: string,
-    public statusCode: number
-  ) {
-    super(message);
-    this.name = this.constructor.name;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
-
-export class BadRequestError extends AppError {
-  constructor(message: string) {
-    super(message, 400);
-  }
-}
-
-export class InternalServerError extends AppError {
-  constructor(message: string) {
-    super(message, 500);
-  }
-}
