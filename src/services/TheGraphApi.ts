3b40e276ea84cf004617d27c343a9e2dcdf2d60c
woutersls <phaerocode@hotmail.com>
2025-05-26 23:19:37 +0200

    refactor: remove unnecessary abstraction

--- src/services/TheGraphApi.ts
+++ src/services/TheGraphApi.ts
@@ -4,18 +4,7 @@ import { PoolKey } from "../models/blockchain/uniswap-v4/uniswap-v4-types";
 export class TheGraphApi {
   constructor(private readonly apiKey: string) {}
 
-  private getV4SubgraphUrl(chain: ChainType): string {
-    switch (chain) {
-      case ChainType.ETH:
-        return "https://gateway.thegraph.com/api/subgraphs/id/DiYPVdygkfjDWhbxGSqAQxwBKmfKnkWQojqeM2rkLb3G";
-      case ChainType.ARB:
-        return "https://gateway.thegraph.com/api/subgraphs/id/G5TsTKNi8yhPSV7kycaE23oWbqv9zzNqR49FoEQjzq1r";
-      case ChainType.BASE:
-        return "https://gateway.thegraph.com/api/subgraphs/id/HNCFA9TyBqpo5qpe6QreQABAA1kV8g46mhkCcicu6v2R";
-      default:
-        throw new Error("Unsupported chain");
-    }
-  }
+  async getTopUniV3Pool(chain: ChainType, tokenAddress: string) {}
 
   /**
    * Fetch pool for a token pair from the Uniswap V4 subgraph.
@@ -26,6 +15,22 @@ export class TheGraphApi {
    * @returns The pool for the token pair.
    */
   async fetchV4PoolKeysByTokens(chain: ChainType, token0: string, token1: string, first = 1000): Promise<PoolKey[]> {
+    let uniV4SubgraphUrl;
+
+    switch (chain) {
+      case ChainType.ETH:
+        uniV4SubgraphUrl = "https://gateway.thegraph.com/api/subgraphs/id/DiYPVdygkfjDWhbxGSqAQxwBKmfKnkWQojqeM2rkLb3G";
+        break;
+      case ChainType.ARB:
+        uniV4SubgraphUrl = "https://gateway.thegraph.com/api/subgraphs/id/G5TsTKNi8yhPSV7kycaE23oWbqv9zzNqR49FoEQjzq1r";
+        break;
+      case ChainType.BASE:
+        uniV4SubgraphUrl = "https://gateway.thegraph.com/api/subgraphs/id/HNCFA9TyBqpo5qpe6QreQABAA1kV8g46mhkCcicu6v2R";
+        break;
+      default:
+        return [];
+    }
+
     const gql = `
       query PoolsByTokens($t0: String!, $t1: String!) {
         pools(where: { token0: $t0, token1: $t1 }, first: ${first}) {
@@ -36,13 +41,12 @@ export class TheGraphApi {
       }
     `;
 
-    const url = this.getV4SubgraphUrl(chain);
     const authHeaders: Record<string, string> = {
       "content-type": "application/json",
       Authorization: `Bearer ${this.apiKey}`,
     };
 
-    const res = await fetch(url, {
+    const res = await fetch(uniV4SubgraphUrl, {
       method: "POST",
       headers: authHeaders,
       body: JSON.stringify({ query: gql, variables: { t0: token0, t1: token1 } }),
