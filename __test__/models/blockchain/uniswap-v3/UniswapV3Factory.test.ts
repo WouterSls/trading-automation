49e79e2330c0e4409617f9744dd1cb6bff2a433b
woutersls <phaerocode@hotmail.com>
2025-05-26 20:33:14 +0200

    refactor: clean up tests

--- __test__/models/blockchain/uniswap-v3/UniswapV3Factory.test.ts
+++ __test__/models/blockchain/uniswap-v3/UniswapV3Factory.test.ts
@@ -8,7 +8,7 @@ const MISSING_PROVIDER_ERROR_MESSAGE = "Wallet has missing provider";
 const INVALID_NETWORK_ERROR_MESSAGE = "Wallet and factory are on different networks";
 const POOL_NOT_FOUND_ERROR_PREFIX = "Pool not found for";
 
-describe("ETH UniswapV3Factory Config", () => {
+describe("ETH UniswapV3Factory", () => {
   let factory: UniswapV3Factory;
   let chainConfig: ChainConfig;
 
@@ -32,20 +32,16 @@ describe("ETH UniswapV3Factory Config", () => {
 
   describe("Get Pool Address", () => {
     it("should return null when pool doesn't exist", async () => {
-      // Arrange
       const tokenA = chainConfig.tokenAddresses.weth;
       const tokenB = wallet.address;
       const feeTier = FeeAmount.MEDIUM;
 
-      // Act
       const poolAddress = await factory.getPoolAddress(wallet, tokenA, tokenB, feeTier);
 
-      // Assert
       expect(poolAddress).toEqual(null);
     });
 
     it("should return pool address when pool exists", async () => {
-      // Arrange
       const PEPE_ADDRESS = "0x6982508145454ce325ddbe47a25d4ec3d2311933";
       const tokenA = PEPE_ADDRESS;
       const tokenB = chainConfig.tokenAddresses.weth;
@@ -54,30 +50,24 @@ describe("ETH UniswapV3Factory Config", () => {
       const V3_POOL_PEPE_ETH_3000_ADDRESS = "0x11950d141EcB863F01007AdD7D1A342041227b58";
       const expectedPoolAddress = V3_POOL_PEPE_ETH_3000_ADDRESS;
 
-      // Act
       const actualPoolAddress = await factory.getPoolAddress(wallet, tokenA, tokenB, feeTier);
 
-      // Assert
       expect(actualPoolAddress).toEqual(expectedPoolAddress);
     });
   });
 
   describe("Get Pool", () => {
     it("should return null when pool doesn't exist", async () => {
-      // Arrange
       const tokenA = chainConfig.tokenAddresses.weth;
       const tokenB = wallet.address;
       const feeTier = FeeAmount.MEDIUM;
 
-      // Act
       const poolAddress = await factory.getPoolAddress(wallet, tokenA, tokenB, feeTier);
 
-      // Assert
       expect(poolAddress).toEqual(null);
     });
 
     it("should return UniswapV3Pool when pool exists", async () => {
-      // Arrange
       const PEPE_ADDRESS = "0x6982508145454ce325ddbe47a25d4ec3d2311933";
       const tokenA = PEPE_ADDRESS;
       const tokenB = chainConfig.tokenAddresses.weth;
@@ -86,10 +76,8 @@ describe("ETH UniswapV3Factory Config", () => {
       const V3_POOL_PEPE_ETH_3000_ADDRESS = "0x11950d141EcB863F01007AdD7D1A342041227b58";
       const expectedPoolAddress = V3_POOL_PEPE_ETH_3000_ADDRESS;
 
-      // Act
       const pool = await factory.getPool(wallet, tokenA, tokenB, feeTier);
 
-      // Assert
       expect(pool).toBeInstanceOf(UniswapV3Pool);
       expect(pool.getPoolAddress()).toBe(expectedPoolAddress);
     });
