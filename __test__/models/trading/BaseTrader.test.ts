3629a792844f69bbfa36b71459d7fd106009dd4e
woutersls <phaerocode@hotmail.com>
2025-05-26 20:32:57 +0200

    test: scaffold trader tests per network

---
+++ __test__/models/trading/BaseTrader.test.ts
@@ -0,0 +1,32 @@
+import { Wallet } from "ethers";
+import { ChainType } from "../../../src/config/chain-config";
+import { Trader } from "../../../src/models/trading/Trader";
+import { TraderFactory } from "../../../src/models/trading/TraderFactory";
+import { NetworkForkManager } from "../../helpers/network-fork";
+import { getHardhatWallet_1 } from "../../../src/hooks/useSetup";
+
+describe("Base Trader", () => {
+  let wallet: Wallet;
+  let trader: Trader;
+
+  beforeAll(async () => {
+    const chain = ChainType.BASE;
+    await NetworkForkManager.startHardhatFork(chain);
+    wallet = getHardhatWallet_1();
+    trader = await TraderFactory.createTrader(wallet);
+  });
+
+  afterAll(async () => {
+    await NetworkForkManager.cleanupHardhatFork();
+  });
+
+  describe("Network validation", () => {
+    it("should create a trader on Base", async () => {
+      const expectedChain = ChainType.BASE;
+
+      const actualChain = await trader.getChain();
+
+      expect(actualChain).toBe(expectedChain);
+    });
+  });
+});
