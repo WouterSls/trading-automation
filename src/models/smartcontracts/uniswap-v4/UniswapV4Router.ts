ee804fb270ba016391e25c9896e3376fd34d7827
wsels <phaerocode@hotmail.com>
2025-06-09 09:41:55 +0200

    chore: scaffold V4 router

---
+++ src/models/smartcontracts/uniswap-v4/UniswapV4Router.ts
@@ -0,0 +1,11 @@
+import { Contract, Wallet } from "ethers";
+
+/** 
+ * @description UniswapV4Router is a helper class to encode transaction data to use in the universal router.
+ * It's not a deployed contract, it acts as an intermediary between the universal router and the uniswap v4 for swaps.
+ */
+//TODO: Static class?
+export class UniswapV4Router {
+  constructor() {
+  }
+}
