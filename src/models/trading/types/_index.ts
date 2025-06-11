0bc1b4382128addead59ead2fddb7f79e675a0c8
woutersls <phaerocode@hotmail.com>
2025-06-11 23:20:30 +0200

    refactor: rename file

--- src/models/trading/types/_index.ts
+++ src/models/trading/types/_index.ts
@@ -2,7 +2,7 @@ export * from "./BuyTrade";
 export * from "./SellTrade";
 export * from "./OutputToken";
 export * from "./InputType";
-export * from "./TradeQuote";
+export * from "./quoting-types";
 export * from "./dto/BuyTradeCreationDto";
 export * from "./dto/SellTradeCreationDto";
 export * from "./dto/WalletCreationDto";
