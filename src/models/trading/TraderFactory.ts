540407f97c1c782fb6c7620914137c5c242b7646
woutersls <phaerocode@hotmail.com>
2025-07-01 23:38:41 +0200

    chore: commented out unimplemented strategies

--- src/models/trading/TraderFactory.ts
+++ src/models/trading/TraderFactory.ts
@@ -18,8 +18,8 @@ export class TraderFactory {
     switch (chain) {
       case ChainType.ETH:
         strategies.push(new UniswapV2Strategy(`UniswapV2-${chain}`, chain));
-        strategies.push(new UniswapV3Strategy(`UniswapV3-${chain}`, chain));
-        strategies.push(new UniswapV4Strategy(`UniswapV4-${chain}`, chain));
+        //strategies.push(new UniswapV3Strategy(`UniswapV3-${chain}`, chain));
+        //strategies.push(new UniswapV4Strategy(`UniswapV4-${chain}`, chain));
         // Add more strategies as needed
         // strategies.push(new SushiswapV2Strategy(`SushiswapV2-${chain}`, chain));
         // strategies.push(new BalancerStrategy(``Balancer-${chain}`, chain));
@@ -32,8 +32,8 @@ export class TraderFactory {
 
       case ChainType.BASE:
         strategies.push(new UniswapV2Strategy(`UniswapV2-${chain}`, chain));
-        strategies.push(new UniswapV3Strategy(`UniswapV3-${chain}`, chain));
-        strategies.push(new AerodromeStrategy(`Aerodrome-${chain}`, chain));
+        //strategies.push(new UniswapV3Strategy(`UniswapV3-${chain}`, chain));
+        //strategies.push(new AerodromeStrategy(`Aerodrome-${chain}`, chain));
         break;
     }
 
