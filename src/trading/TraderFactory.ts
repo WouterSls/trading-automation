a03cefa43fd46e182185742fefd97df9712f08f4
wsels <phaerocode@hotmail.com>
2025-07-09 17:59:06 +0200

    chore: add uniV4 strategy creation to trader factory

--- src/trading/TraderFactory.ts
+++ src/trading/TraderFactory.ts
@@ -19,7 +19,7 @@ export class TraderFactory {
       case ChainType.ETH:
         strategies.push(new UniswapV2Strategy(`UniswapV2-${chain}`, chain));
         strategies.push(new UniswapV3Strategy(`UniswapV3-${chain}`, chain));
-        //strategies.push(new UniswapV4Strategy(`UniswapV4-${chain}`, chain));
+        strategies.push(new UniswapV4Strategy(`UniswapV4-${chain}`, chain));
         // Add more strategies as needed
         // strategies.push(new SushiswapV2Strategy(`SushiswapV2-${chain}`, chain));
         // strategies.push(new BalancerStrategy(``Balancer-${chain}`, chain));
@@ -28,11 +28,13 @@ export class TraderFactory {
       case ChainType.ARB:
         strategies.push(new UniswapV2Strategy(`UniswapV2-${chain}`, chain));
         strategies.push(new UniswapV3Strategy(`UniswapV3-${chain}`, chain));
+        strategies.push(new UniswapV4Strategy(`UniswapV4-${chain}`, chain));
         break;
 
       case ChainType.BASE:
         strategies.push(new UniswapV2Strategy(`UniswapV2-${chain}`, chain));
         strategies.push(new UniswapV3Strategy(`UniswapV3-${chain}`, chain));
+        strategies.push(new UniswapV4Strategy(`UniswapV4-${chain}`, chain));
         strategies.push(new AerodromeStrategy(`Aerodrome-${chain}`, chain));
         break;
     }
