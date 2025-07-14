b51f314956e8ddd15badb97c0f6e92683a8899f3
wsels <phaerocode@hotmail.com>
2025-07-14 13:36:31 +0200

    refactor: extra trading utils to trading utils file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,8 +7,6 @@ import { UNISWAP_V2_PAIR_INTERFACE } from "./smartcontract-abis/uniswap-v2";
 import { calculatePriceFromSqrtPriceX96 } from "../smartcontracts/uniswap-v3/uniswap-v3-utils";
 import { UNIVERSAL_ROUTER_INTERFACE } from "./smartcontract-abis/universal-router";
 import { TradeCreationDto } from "../trading/types/dto/TradeCreationDto";
-import { InputType, TradeType } from "../trading/types/trading-types";
-import { TRADING_CONFIG } from "../config/trading-config";
 
 export async function validateNetwork(wallet: Wallet, chainType: ChainType) {
   try {
@@ -226,44 +224,6 @@ function analyzeErrorParams(errorData: string) {
     analysis: `${paramCount} parameters detected`,
     parameters: params,
   };
-}
-
-export function determineTradeType(trade: TradeCreationDto): TradeType {
-  const isTokenInput = trade.inputType === InputType.TOKEN && trade.inputToken !== ethers.ZeroAddress;
-  const isEthInput = trade.inputType === InputType.ETH && trade.inputToken === ethers.ZeroAddress;
-  const isUsdInput = trade.inputType === InputType.USD && trade.inputToken === ethers.ZeroAddress;
-  const isTokenOutput = trade.outputToken !== ethers.ZeroAddress;
-  const isEthOutput = trade.outputToken === ethers.ZeroAddress;
-
-  if ((isEthInput || isUsdInput) && isTokenOutput) {
-    return TradeType.ETHInputTOKENOutput;
-  }
-
-  if (isTokenInput && isEthOutput) {
-    return TradeType.TOKENInputETHOutput;
-  }
-
-  if (isTokenInput && isTokenOutput) {
-    return TradeType.TOKENInputTOKENOutput;
-  }
-
-  if ((isEthInput || isUsdInput) && isEthOutput) {
-    throw new Error("Can't trade ETH -> ETH");
-  }
-
-  throw new Error("Unknown trade type for given TradeCreationDto");
-}
-
-export function calculatePriceImpact(expectedOutput: bigint, actualOutput: bigint) {
-  if (expectedOutput === 0n) {
-    return 0n;
-  }
-
-  return ((expectedOutput - actualOutput) / expectedOutput) * 100n;
-}
-
-export function calculateSlippageAmount(actualOutput: bigint) {
-  return (actualOutput * BigInt(TRADING_CONFIG.SLIPPAGE_TOLERANCE * 100)) / 100n;
 }
 
 export function displayTrade(trade: TradeCreationDto) {
