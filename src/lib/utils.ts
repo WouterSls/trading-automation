43b068c624e281eb4cd3b681b32de322e43e8a3e
woutersls <phaerocode@hotmail.com>
2025-08-18 16:44:22 +0200

    chore: update error and add displaying functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,22 +7,25 @@ import { UNISWAP_V2_PAIR_INTERFACE } from "./smartcontract-abis/uniswap-v2";
 import { calculatePriceFromSqrtPriceX96 } from "../smartcontracts/uniswap-v3/uniswap-v3-utils";
 import { UNIVERSAL_ROUTER_INTERFACE } from "./smartcontract-abis/universal-router";
 import { TradeCreationDto } from "../trading/types/dto/TradeCreationDto";
-import { InputType, TradeType } from "../trading/types/trading-types";
+import { InputType, TradeConfirmation, TradeType } from "../trading/types/trading-types";
 import { TRADING_CONFIG } from "../config/trading-config";
+import { ValidationError } from "./errors";
+import { getCoingeckoApi } from "../hooks/useSetup";
+import { ERC20 } from "../smartcontracts/ERC/ERC20";
 
 export async function validateNetwork(wallet: Wallet, chainType: ChainType) {
   try {
     const network = await wallet.provider!.getNetwork();
     const chain = mapNetworkNameToChainType(network.name);
     if (chain !== chainType) {
-      throw new Error(`Wallet on different chain`);
+      throw new ValidationError(`Wallet on different chain`);
     }
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : "Unknown error has occurred";
     if (errorMessage.toLowerCase().includes("wallet on different chain")) {
       throw error;
     }
-    throw new Error("Network Validation Failed");
+    throw new ValidationError("Network Validation Failed");
   }
 }
 
@@ -277,6 +280,21 @@ export function displayTrade(trade: TradeCreationDto) {
   console.log();
 }
 
+export function displayTradeConfirmation(tradeConfirmation: TradeConfirmation) {
+  console.log("--------------------------------");
+  console.log("Trade Confirmation");
+  console.log("--------------------------------");
+  console.log("\tStrategy", tradeConfirmation.quote.strategy);
+  console.log("\tRoute: ", tradeConfirmation.quote.route.path);
+  console.log("\tGas Spent:", tradeConfirmation.gasCost);
+  console.log("\tETH Spent:", tradeConfirmation.ethSpentFormatted);
+  console.log("\tETH Received:", tradeConfirmation.ethReceivedFormatted);
+  console.log("\tTokens Spent:", tradeConfirmation.tokensSpentFormatted);
+  console.log("\tTokens Received:", tradeConfirmation.tokensReceivedFormatted);
+  console.log("\tTransaction Hash:", tradeConfirmation.transactionHash);
+  console.log();
+}
+
 export async function displayTokenBalance(tokenAddress: string, wallet: Wallet) {
   const balanceOfTxData = await ERC20_INTERFACE.encodeFunctionData("balanceOf", [wallet.address]);
   const balanceOfTx: TransactionRequest = {
@@ -287,4 +305,13 @@ export async function displayTokenBalance(tokenAddress: string, wallet: Wallet) 
 
   const rawResult = ERC20_INTERFACE.decodeFunctionResult("balanceOf", result);
   console.log(`\t${tokenAddress} | ${rawResult}`);
+}
+
+export async function displayLivePrice(chain: ChainType, token: ERC20) {
+  const geckoTerminalApi = getCoingeckoApi();
+
+  const liveUsdPrice = await geckoTerminalApi.getTokenUsdPrice(chain, token.getTokenAddress());
+  console.log(`${token.getName()} | ${token.getTokenAddress()}`);
+  console.log(`$${liveUsdPrice}`);
+  console.log();
 }
